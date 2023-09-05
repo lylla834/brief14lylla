668fd80af89dbe5b7547ae475264982b35de71dc
lylla834 <123461723+lylla834@users.noreply.github.com>
2023-09-05 12:29:53 +0200

    Update init-script.sh

--- init-script.sh
+++ init-script.sh
@@ -5,7 +5,10 @@
 sudo apt-get update
 sudo apt install docker.io -y
 sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker lylla
+sudo chown lylla:docker /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
